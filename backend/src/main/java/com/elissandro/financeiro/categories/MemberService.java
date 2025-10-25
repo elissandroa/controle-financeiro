798efc306291eadd6af49a56bc88bf9f13797d0b
Elissandro Aparecido Anastácio <58570300+elissandroa@users.noreply.github.com>
2025-10-24 23:07:47 -0300

    MemberService, MemberController

---
+++ backend/src/main/java/com/elissandro/financeiro/categories/MemberService.java
@@ -0,0 +1,51 @@
+package com.elissandro.financeiro.categories;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.elissandro.financeiro.dto.MemberDTO;
+import com.elissandro.financeiro.entities.Member;
+import com.elissandro.financeiro.repositories.MemberRepository;
+
+@Service
+public class MemberService {
+
+	@Autowired
+	private MemberRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<MemberDTO> findAll() {
+		List<Member> members = repository.findAll();
+		return members.stream().map(mem -> new MemberDTO(mem)).toList();
+	}
+
+	public MemberDTO save(MemberDTO member) {
+		Member newMember = new Member();
+		newMember.setName(member.getName());
+		newMember.setRole(member.getRole());
+		newMember.setCreatedAt(member.getCreatedAt() != null ? member.getCreatedAt() : LocalDate.now());
+		newMember = repository.save(newMember);
+		return new MemberDTO(newMember);
+	}
+
+	
+	public MemberDTO update(Long id, MemberDTO memberDetails) {
+		Member member = repository.findById(id).orElseThrow(() -> new RuntimeException("Member not found"));
+
+		member.setName(memberDetails.getName());
+		member.setRole(memberDetails.getRole());
+		member = repository.save(member);
+		return memberDetails;
+	}
+
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new RuntimeException("Member not found");
+		}
+		repository.deleteById(id);
+	}
+}
---
+++ backend/src/main/java/com/elissandro/financeiro/controllers/MemberController.java
@@ -0,0 +1,44 @@
+package com.elissandro.financeiro.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.elissandro.financeiro.categories.MemberService;
+import com.elissandro.financeiro.dto.MemberDTO;
+
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+
+	@Autowired
+	private MemberService service;
+	
+	@GetMapping
+	public List<MemberDTO> findAll() {
+		return service.findAll();
+	}
+	
+	@PostMapping
+	public MemberDTO createMember(@RequestBody MemberDTO memberDTO) {
+		return service.save(memberDTO);
+	}
+	
+	@PutMapping("/{id}")
+	public MemberDTO updateMember(@PathVariable Long id, @RequestBody MemberDTO memberDTO) {
+		return service.update(id, memberDTO);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteMember(@PathVariable Long id) {
+		service.delete(id);
+	}
+}
