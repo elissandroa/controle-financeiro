8bd8e48da0967cfdbd040b28d978c9623752f1c9
Elissandro Aparecido Anastácio <58570300+elissandroa@users.noreply.github.com>
2025-10-24 22:46:52 -0300

    CategoryService, CategoryController

---
+++ backend/src/main/java/com/elissandro/financeiro/categories/CategoryService.java
@@ -0,0 +1,55 @@
+package com.elissandro.financeiro.categories;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.elissandro.financeiro.dto.CategoryDTO;
+import com.elissandro.financeiro.entities.Category;
+import com.elissandro.financeiro.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> categories = repository.findAll();
+		return categories.stream().map(cat -> new CategoryDTO(cat)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Category category = repository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Category not found"));
+		return new CategoryDTO(category);
+	}
+	
+	public CategoryDTO save(CategoryDTO category) {
+		Category newCategory = new Category();
+		newCategory.setName(category.getName());
+		newCategory = repository.save(newCategory);
+		return new CategoryDTO(newCategory);
+	}
+	
+	public CategoryDTO update(Long id, CategoryDTO categoryDetails) {
+		Category category = repository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Category not found"));
+		
+		category.setName(categoryDetails.getName());
+		category = repository.save(category);
+		return categoryDetails;
+	}
+	
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new RuntimeException("Category not found");
+		}
+		repository.deleteById(id);
+	}
+
+}
--- backend/src/main/java/com/elissandro/financeiro/repositories/CategoryRepository.java
+++ backend/src/main/java/com/elissandro/financeiro/repositories/CategoryRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.elissandro.financeiro.entities.Category;
 
-public interface CatetoryRepository extends JpaRepository<Category, Long> { 
+public interface CategoryRepository extends JpaRepository<Category, Long> { 
 
 }
